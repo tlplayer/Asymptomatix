a7dc49e03145f05b732db51154f1bccc05fc52c8
Tim <timothy.player@comcast.net>
2021-04-11 13:45:00 -0400

    Commit

---
+++ flaskr/geoform.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import Form, FieldList, FormField, FloatField, SelectField, \
+        StringField, TextAreaField, SubmitField
+from wtforms import validators, fields
+
+
+class GeoForm(Form):
+    """Sub Form
+        This takes the longitude and latitude of the user's that are positve for covid for the last couple of days.
+    """
+    longitude = FloatField(
+        'Longitude',
+        validators=[validators.InputRequired()]
+    )
+    latitude = FloatField(
+        'Latitude',
+        validators=[validators.InputRequired()]
+    )
+    #Year month day format
+    time = StringField(
+        'Date',
+        validators=[validators.InputRequired(), validators.Length(min=10,max=10)]
+    ) 
+
+
+class MainForm(FlaskForm):
+    """Parent form. Takes 1-20 forms"""
+    locations = FieldList(
+        FormField(GeoForm),
+        min_entries=1,
+        max_entries=20
+    )
