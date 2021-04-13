a7ab7645be90637b15a8ff51142fabc8b79c929f
Elliott Perryman <dperrym2@lbl.gov>
2021-04-13 09:38:48 -0400

    maybe there's a better way to read the keys?

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,12 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from flaskr import config
 from flask_googlemaps import GoogleMaps
+# make key.py with API_KEY='your_api_string'
 from flaskr import key
 
+print('key:',key)
 # These are the configurations we need for flask and SQLite
 app = Flask(__name__)
 app.config.from_object(config.Config)
-app.config['GOOGLEMAPS_KEY'] = key
+app.config['GOOGLEMAPS_KEY'] = key.API_KEY
 db = SQLAlchemy(app)
 db.init_app(app)
 
@@ -20,3 +22,5 @@ db.init_app(app)
 engine = create_engine('sqlite:///cases.db', echo = True)
 
 from flaskr import routes
+
+
