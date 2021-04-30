cc8b626cac7f9730f4e048b11244a82453fe5203
abryan40 <abryan40@vols.utk.edu>
2021-04-29 21:36:14 -0400

    Initial working heatmap
    A multitude of changes to file topology, and added generation of heatmaps with call to the generate_hotspots function via an a(s)jax request in mapview.html

---
+++ flaskr/data/datasplitter.py
@@ -0,0 +1,38 @@
+import json
+import os
+import sys
+
+dirname = os.path.dirname(__file__)
+os.chdir(dirname)
+
+src = 'fake_CT10000_wvax.json'
+srcfile = src
+dst = 'day'
+print(os.getcwd())
+with open(srcfile,'r') as f:
+    data = json.load(f)
+
+running = True
+times = data[0]['t']
+splitdata = []
+for time in times:
+    i = times.index(time)
+    for element in data:
+        if time in element['t']:
+            splitdata.append({
+                'id' : element['id'],
+                'pos' : element['positive'],
+                'vax' : element['vaccinated'],
+                'coord' : (element['lat'][i],element['lon'][i])
+            })
+    try:
+        dstfile = dst+str(int(time/1440))+'/'
+        if not os.path.isdir(dstfile):
+            os.mkdir(dstfile)
+        dstfile += 't'+str(int(time%1440))+'.json'
+        with open(dstfile,'w') as f:
+            json.dump(splitdata,f,indent=4)
+    except IOError:
+        print("Failed to open ",dstfile,file=sys.stderr)
+        exit()
+    splitdata.clear()
--- flaskr/routes.py
+++ flaskr/routes.py
@@ -7,7 +7,7 @@ by the URL or links.
 '''
 
 # This file holds the URLs and the logic for each.
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, make_response
 from markupsafe import escape
 from flaskr import app
 from flaskr import geoform
@@ -16,6 +16,11 @@ from flaskr import database
 from flask_googlemaps import Map
 from flaskr import GoogleMaps
 from flaskr.test_data import x_y_tuples
+from flask import json as flask_json
+import json
+from numpy import sqrt
+from os import path
+
 
 GoogleMaps(app)
 
@@ -91,3 +96,55 @@ def form(name=None):
         form=form,
         _template=template_form
         )
+
+@app.route('/generatemap/<filename>',methods=['GET','POST'])
+def generate_hotspots(filename="./data/exampledata.json"):
+    filename = path.join(app.root_path+'/data/day0/'+filename)
+    hotspots = {}
+    hs_num = 0
+                            # 0.0001 for 36 foot radius and 0.0002 for
+    PROXIMITY = 0.000001    # 100 foot radius, thereabouts
+    FORM = 10               # scale for degree decimal used in data
+    PROXIMITY = PROXIMITY * FORM
+    c = 'coord'
+    pos = 'pos'
+    with open(filename,'r') as f:
+        data = json.load(f)
+    data = sorted(data,key=lambda x: (x[c][0],x[c][1]))
+    for i in range(len(data)):
+        if(data[i][pos]):
+            # Scan the people within PROXIMITY to determine possible infection
+            search_width = 10
+            start = -1
+            # Iteratively increase the number of people we need to search over
+            # Order logN  * n for determining infections. Better than n^2
+            while(not(i - search_width <= 0)):
+                if(data[i-search_width][c][0] < data[i][c][0] - PROXIMITY):
+                    start = i-search_width
+                    break
+                search_width *= 2
+            if(start == -1):
+                start = 0
+            end = -1
+            search_width = 10
+            while(not(i + search_width >= len(data)-1)):
+                if(data[i-search_width][c][0] > data[i][c][0] + PROXIMITY):
+                    end = i+search_width
+                    break
+                search_width *= 2
+            if(end == -1):
+                end = len(data)-1
+
+            # If there is a possible exposure, add exposure midpoint
+            for j in range(start,end):
+                y = data[j][c]
+                x = data[i][c]
+                if(sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2) < PROXIMITY):
+                    # Add lat,long as tuple to meaningless incrementing int key, to return as dict
+                    hotspots[hs_num] = ((x[0]+y[0])/2,(x[1]+y[1])/2)
+                    hs_num += 1
+    # Return latitude and longitude coords
+    headers = {"Content-Type" : "application/json"}
+    return make_response(
+        hotspots
+    )
